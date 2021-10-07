63058c57d3d80fe90487efb7fb002d031ee7ba3e
Denis Kuznetosv <kuznetsov.den.p@gmail.com>
2021-10-07 19:22:40 +0300

    main: add speech_function_classifier

---
+++ annotators/speech_function_classifier/test_server.py
@@ -0,0 +1,28 @@
+import requests
+import os
+
+
+SERVICE_PORT = os.getenv("SERVICE_PORT")
+URL = f"http://0.0.0.0:{SERVICE_PORT}/model"
+
+
+def handler(requested_data):
+    hypothesis = requests.post(URL, json=requested_data).json()
+    return hypothesis
+
+
+def run_test(handler):
+    hypothesis = handler(
+        {
+            "phrase": ["fine, thank you.", "and you?"],
+            "prev_phrase": "How are you doing today?",
+            "prev_speech_function": "Open.Demand.Fact",
+        }
+    )
+    print(f"test name: {hypothesis}")
+    assert hypothesis == [['React.Rejoinder.Support.Response.Resolve', 'React.Rejoinder.Confront.Response.Re-challenge']]
+    print("Success")
+
+
+if __name__ == "__main__":
+    run_test(handler)
