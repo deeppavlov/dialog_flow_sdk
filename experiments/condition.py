0ec5d3cfdb24649486aaf166f9e6213d2b5ed575
Daniel Kornev <daniel@zetuniverse.com>
2021-10-14 10:54:37 +0300

    even more good changes

--- experiments/condition.py
+++ experiments/condition.py
@@ -7,9 +7,15 @@ logger = logging.getLogger(__name__)
 
 def is_sf(sf_name="Open.Give.Opinion"):
     def is_sf_handler(ctx: Context, actor: Actor, *args, **kwargs):
-        return sf_name in ctx.misc.get("speech_functions", [[]])[-1]
+        return sf_name in ctx.misc.get("speech_functions", [[""]])[-1][-1]
 
     return is_sf_handler
+
+def is_midas(midas_name="pos_answer", treshhold=0.5):
+    def is_midas_handler(ctx: Context, actor: Actor, *args, **kwargs):
+        return midas_name in [key for key, val in ctx.misc.get("midas", [{}])[-1].items() if val > treshhold]
+
+    return is_midas_handler
 
 
 speech_functions = is_sf
